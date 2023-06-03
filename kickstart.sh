3b9c0f7b9bdd195e999b1b47e8092da7f436f476
cwillsey06 <coltranew@tuta.io>
2023-06-03 02:22:35 -0600

    boilerplate, boilerplate
    signing off for now

--- kickstart.sh
+++ kickstart.sh
@@ -10,22 +10,53 @@
 
 # For more information, please refer to <https://unlicense.org>
 
-PKGS="pipewire alsa-pipewire wireplumber dbus-elogind polkit git clang clang-tools-extra meson ninja ccache curl autoconf automake make ncurses ncurses-devel pkg-config bc xorg-minimal xorg-server xorg-fonts firefox neovim mpv xtools xclip ranger scrot tmux gimp zsh"
+PKGS="pipewire alsa-pipewire wireplumber dbus-elogind polkit git clang clang-tools-extra meson ninja ccache curl autoconf automake make ncurses ncurses-devel pkg-config bc xorg-minimal xorg-server xorg-fonts hsetroot firefox neovim mpv xtools xclip ranger scrot tmux gimp zsh"
 
 elevate () {
 	if [ "$(whoami)" = 'root' ]; then
 		"$*"
 	else
-		su -c "$*"
+		sudo "$*"
 	fi
 }
 
-install_pkgs () {
-	elevate xbps-install "$PKGS"
+clone_repos () {
+	mkdir -p "$HOME/src"
+	git clone 'https://github.com/cwillsey06/suckless-cw' "$HOME/src/suckless"
+}
+
+build_software () {
+	cd "$HOME/src/suckless"
+	git submodule update --remote
+	find "$(pwd -P)" -maxdepth 1 -not -path '*/.git' -type d \
+		-exec sh -c 'cd {} && make' ';'
+}
+
+configure_all () {
+	mkdir -p "$HOME/src"
+	# xinitrc
+	cat <<EOF > "$HOME/.xinitrc"
+#!/bin/sh
+export "$(dbus-launch)"
+hsetroot -solid '#282828' \
+	-cover "$(find "$HOME/.local/share/backgrounds" -maxdepth 1 -type l -name '*active*')" \
+	2> /dev/null
+exec pipewire &
+exec slstatus &
+exec dwm
+EOF
+	cat <<EOF > "$HOME"
+
+EOF
+	chsh --shell /bin/zsh
+	rm -f "$HOME/.bash*"
 }
 
 main () {
-	elevate xbps-install -Su
+	elevate xbps-install -Su "$PKGS"
+	clone_repos
+	build_software
+	configure_all
 }
 
 main "$@";
